4dbc068a8d560bf37dbe09ef8561a4372e1422e8
Ben <swanzeyb2001@gmail.com>
2022-03-12 20:20:52 -0800

    Find dominant color of image

--- lib/ocr-image/preprocess.py
+++ lib/ocr-image/preprocess.py
@@ -18,22 +18,50 @@ height = src.shape[0]
 # Binary image
 ret, img = cv.threshold(src, 180, 255, cv.THRESH_BINARY)
 
-# Find boxes
-def getSigBoxes(img, sigWidth):
+# Find bounding bpxes
+def getBoundingBoxes(img):
   contours, _ = cv.findContours(img, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
   contours_poly = [None]*len(contours)
   boundRect = [None]*len(contours)
   for i, c in enumerate(contours):
     contours_poly[i] = cv.approxPolyDP(c, 3, True)
     boundRect[i] = cv.boundingRect(contours_poly[i])
-  boxes = []
-  for box in boundRect:
-    if box[2] >= sigWidth:
-      if box[2] != width:
-        boxes.append(box)
-  return boxes
+  return boundRect
 
-boxes = getSigBoxes(img, width * 0.2)
+
+# Filter to large bounding boxes
+sigWidth = 0.2
+allBoxes = getBoundingBoxes(img)
+
+sigBoxes = []
+for box in allBoxes:
+  if box[2] > width * sigWidth:
+    sigBoxes.append(box)
+
+# Get part of image
+def crop(rect):
+  x = rect[0]
+  y = rect[1]
+  w = rect[2]
+  h = rect[3]
+  return img[y:y+h, x:x+w]
+
+# Find boxes with a mostly black background
+def dominantColor(img):
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)     
+        img = img.reshape((img.shape[0] * img.shape[1], 3))
+        kmeans = KMeans(n_clusters = self.CLUSTERS)
+        kmeans.fit(img)
+        self.COLORçcluster_centers_
+        self.LABELS = kmeans.labels_
+        return self.COLORS.astype(int)
+
+boxes = []
+for rect in sigBoxes:
+  box = crop(rect)
+  dom = unique_count_app(box)
+  if (dom != (255, 255, 255)):
+    boxes.append(rect)
 
 # Invert colors in boxes
 def overlayImage(l_img, s_img, x_offset, y_offset):
@@ -42,9 +70,7 @@ def overlayImage(l_img, s_img, x_offset, y_offset):
 def invertBox(img, rect):
   x = rect[0]
   y = rect[1]
-  w = rect[2]
-  h = rect[3]
-  box = img[y:y+h, x:x+w]
+  box = crop(rect)
   box = cv.bitwise_not(box)
   overlayImage(img, box, x, y)
 
